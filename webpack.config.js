261972c5f483d65d30bd05c1169f25df1800a997
mybrewery <mybrewery@ya.ru>
2018-04-22 18:57:48 +0300

    react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require("webpack");
+
+module.exports = {
+	entry	: [
+		"react-hot-loader/patch",
+		"./src/index.js"
+	],
+	output	: {
+		path		: __dirname + "/dist",
+		publicPath 	: "/",
+		filename	: "bundle.js"
+	},
+	devServer : {
+		contentBase : "./dist",
+		hot : true
+	},
+	module : {
+		"rules": [{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: ["babel-loader"]
+		}]
+	},
+	plugins : [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	resolve : {
+		extensions: ["*", ".js", ".jsx"]
+	} 
+}
